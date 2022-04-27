ab0135bc4206c63f6357caac38460a23b88b8447
cy <7454@qq.com>
2022-04-27 23:28:13 +0800

    用for和while循环求0~100的偶数和

--- Day4/loop_test.py
+++ Day4/loop_test.py
@@ -5,16 +5,43 @@ Version: 0.01
 Author: cy
 """
 """
+#for....in....
 sum = 0
 for x in range(101):
     sum += x
 print('1~100的和是%d' % sum)
 """
 """
+#while....
 sum = 0
 x = 0
 while x < 100 :
     x += 1
     sum += x
 print('1~100的和是%d' % sum)
+"""
+
+
+"""
+用for循环实现1~100之间的偶数求和
+
+Version: 0.01
+Author: cy
+"""
+
+"""
+#for...in....
+sum = 0
+for x in range(0, 101, 2):
+    sum += x
+print('0~100的偶数和为 %d' % sum)
+"""
+"""
+#while....
+sum = 0
+x = 0
+while x < 100 :
+    x += 2
+    sum += x
+print('0~100的偶数和为 %d' % sum)
 """
