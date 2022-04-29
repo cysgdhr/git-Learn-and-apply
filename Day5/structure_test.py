8c24d45f55e49e4e4c6a7bc87c7504c7ed05890f
cy <7454@qq.com>
2022-04-29 09:31:07 +0800

    正整数反转

--- Day5/structure_test.py
+++ Day5/structure_test.py
@@ -4,10 +4,27 @@
 Version: 0.01
 Author: cy
 """
+"""
 for num in range(100, 1000):
     a = num % 10
     b = num // 10 % 10
     c = num // 100
     if a ** 3 + b ** 3 + c ** 3 == num:
         print(num)
+"""
+
+
+"""
+正整数的反转
+
+Version: 0.01
+Author: cy
+"""
+
+num = int(input('请输入一个正整数：'))
+reverse_num = 0
+while num > 0:
+    reverse_num = reverse_num * 10 + num % 10
+    num //= 10
+print('反转之后是%d' % reverse_num)
 
