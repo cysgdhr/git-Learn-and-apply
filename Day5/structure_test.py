6cfbcb7394bfcdd41e2885a6a6b33744634771cb
cy <7454@qq.com>
2022-04-30 09:58:26 +0800

    穷举法百钱百鸡

--- Day5/structure_test.py
+++ Day5/structure_test.py
@@ -20,11 +20,31 @@ for num in range(100, 1000):
 Version: 0.01
 Author: cy
 """
-
+"""
 num = int(input('请输入一个正整数：'))
 reverse_num = 0
 while num > 0:
     reverse_num = reverse_num * 10 + num % 10
     num //= 10
 print('反转之后是%d' % reverse_num)
+"""
 
+"""
+(https://baike.baidu.com/item/%E5%BC%A0%E4%B8%98%E5%BB%BA/10246238)
+百钱百鸡问题
+
+Version: 0.01
+Author: cy
+"""
+"""
+for Rooster in range(20):
+    for Hen in range(33):
+        for Chick in range(300):
+            if(Rooster * 5 + Hen * 3 + Chick / 3 == 100) and (Rooster + Hen + Chick == 100):
+                print("公鸡%d只，母鸡%d只，小鸡%d只" % (Rooster, Hen, Chick))
+"""
+for Rooster in range(20):
+    for Hen in range(33):
+        Chick = 100 - Rooster - Hen
+        if 5 * Rooster + 3 * Hen + Chick / 3 == 100:
+            print('公鸡%d只，母鸡%d只，小鸡%d只' % (Rooster, Hen, Chick))
