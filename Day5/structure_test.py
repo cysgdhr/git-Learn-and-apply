1384eea697c87525e19db1925717d7e8d59466a8
cy <7454@qq.com>
2022-05-01 09:15:45 +0800

    Craps赌博游戏-自己的思路

--- Day5/structure_test.py
+++ Day5/structure_test.py
@@ -43,8 +43,55 @@ for Rooster in range(20):
             if(Rooster * 5 + Hen * 3 + Chick / 3 == 100) and (Rooster + Hen + Chick == 100):
                 print("公鸡%d只，母鸡%d只，小鸡%d只" % (Rooster, Hen, Chick))
 """
+"""
 for Rooster in range(20):
     for Hen in range(33):
         Chick = 100 - Rooster - Hen
         if 5 * Rooster + 3 * Hen + Chick / 3 == 100:
             print('公鸡%d只，母鸡%d只，小鸡%d只' % (Rooster, Hen, Chick))
+"""
+
+"""
+Craps赌博游戏
+我们设定玩家开始游戏时有1000元的赌注
+游戏结束的条件是玩家输光所有的赌
+
+Version: 0.01
+Author: cy
+"""
+from random import randint
+
+Principal = 1000
+Frequency = 1
+while Principal != 0:
+    if Frequency == 1:
+        Bet = int(input("请下注："))
+        while Bet > Principal:
+            print("你的本金剩%d" % Principal)
+            Bet = int(input("请重新下注："))
+
+    Dice1 = randint(1, 6)
+    Dice2 = randint(1, 6)
+    Num = Dice1 + Dice2
+    if Frequency == 1:
+        if Num == 7 or Num == 11:
+            print('骰子1是%d点，骰子2是%d点，总和%d，玩家胜' % (Dice1, Dice2, Num))
+            Principal += Bet
+        elif Num == 2 or Num == 3 or Num == 12:
+            print('骰子1是%d点，骰子2是%d点，总和%d，庄家胜' % (Dice1, Dice2, Num))
+            Principal -= Bet
+        else:
+            print('骰子1是%d点，骰子2是%d点，总和%d，没有赢家，继续摇' % (Dice1, Dice2, Num))
+            Frequency += 1
+            First_num = Num
+    else:
+        if Num == First_num:
+            print('骰子总和是%d，和第一次骰子总和一样，玩家胜' % Num)
+            Principal += Bet
+            Frequency = 1
+        elif Num == 7:
+            print('骰子总和是7，庄家胜')
+            Principal -= Bet
+            Frequency = 1
+print('你输光了')
+
