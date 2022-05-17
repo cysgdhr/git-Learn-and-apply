2cc5ad89e46cbdb8906f9f1c39eb2f9b11e6e0a0
cy <7454@qq.com>
2022-05-17 12:45:48 +0800

    指定长度的验证码函数

--- Day7/string_structure.py
+++ Day7/string_structure.py
@@ -4,6 +4,7 @@
 Version: 0.01
 Author: cy
 """
+"""
 import os
 import time
 
@@ -17,3 +18,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+"""
+
+"""
+设计一个函数产生指定长度的验证码，验证码由大小写字母和数字构成
+：param code_len: 验证码的长度(默认4个字符)
+：return 由大小写英文字符和数字构成的随机验证码
+
+Version: 0.01
+Author: cy
+"""
+
+import random
+def generate_code(code_len = 4):
+
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+def main():
+    print(generate_code(6))
+if __name__ == '__main__':
+    main()
+
+
